c5c7d0203d07e09d308919b52ca241ba33034543
Affaire Conclue <1751720+Affaire-Conclue@users.noreply.github.com>
2025-06-28 12:00:19 +0200

    Fix my push2prod helper script

--- scripts/push2prod.bat
+++ scripts/push2prod.bat
@@ -1,4 +1,22 @@
+@echo off
+echo ===============================================
+echo  Switching to 'prod' branch and merging 'main'
+echo ===============================================
 git checkout prod
 git merge main
+
+echo.
+echo ===============================================
+echo  Pushing 'prod' to origin
+echo ===============================================
 git push origin prod
+
+echo.
+echo ===============================================
+echo  Switching back to 'main' branch
+echo ===============================================
 git checkout main
+
+echo.
+echo  Process complete!
+pause
